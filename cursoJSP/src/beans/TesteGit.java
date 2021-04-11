ce95e25f29e13ebf31960979af787d55d2f920b6
danil <danil@DESKTOP-6C4AJ56>
2021-04-11 19:41:50 -0300

    adicionado campos

--- cursoJSP/src/beans/TesteGit.java
+++ cursoJSP/src/beans/TesteGit.java
@@ -5,4 +5,39 @@ public class TesteGit {
 	private String nome;
 	private String sobrenome;
 	private int idade;
+	
+	private String nasalsda;
+	private boolean asfasdasd;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSobrenome() {
+		return sobrenome;
+	}
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public String getNasalsda() {
+		return nasalsda;
+	}
+	public void setNasalsda(String nasalsda) {
+		this.nasalsda = nasalsda;
+	}
+	public boolean isAsfasdasd() {
+		return asfasdasd;
+	}
+	public void setAsfasdasd(boolean asfasdasd) {
+		this.asfasdasd = asfasdasd;
+	}
+	
+	
 }
